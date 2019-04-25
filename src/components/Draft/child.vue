202a4eccd7641066f4c288236582ef34215cf4bf
shino <shino@zamplus.com>
2019-04-25 09:58:08 +0800

    +

---
+++ src/components/Draft/child.vue
@@ -0,0 +1,45 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+export default {
+  name: 'child',
+  props: {
+    fooCopy: {
+      type: Object,
+      default () {
+        return {
+          index: 1,
+          name: 'foo'
+        }
+      }
+    }
+  },
+  data () {
+    return {
+      foo: {}
+    }
+  },
+  watch: {
+    fooCopy: {
+      handler (nv, ov) {
+        console.log((nv && nv.index) || null, (ov && ov.index) || null)
+        this.foo = nv
+      },
+      deep: true,
+      immediate: true
+    },
+    foo: {
+      handler (nv) {
+        this.$emit('update:foo', nv)
+      },
+      deep: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Draft/trySomething.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="container_trySomething">
+        <child :foo.sync="foo" :fooCopy="foo"/>
+        <button @click="updateFoo">updateFoo</button>
+    </div>
+</template>
+
+<script>
+import child from './child'
+
+export default {
+  name: 'try',
+  components: {
+    child
+  },
+  data () {
+    return {
+      foo: {
+        index: 1,
+        name: 'foo'
+      },
+      timeout: null
+    }
+  },
+  methods: {
+    updateFoo () {
+      this.foo.index++
+      this.foo = { ...this.foo }
+    }
+  },
+  watch: {
+    foo: {
+      handler (nv, ov) {
+        // console.log(nv.index, ov.index)
+        console.count('foo')
+      },
+      deep: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
